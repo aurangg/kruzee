e05542057978b5cd12f895f2050cc8fa05cf0693
Zohaib <zohaib.dilshad.1@gmail.com>
2022-07-04 19:23:25 +0500

    change stripe key

---
+++ src/Components/Common/Up.js
@@ -0,0 +1,12 @@
+// (function (u, p, l, i, f, t, o, b, j) {
+
+//     u['UpLiftPlatformObject'] = f;
+
+//  u[f] = u[f] || function () { (u[f].q = u[f].q || []).push(arguments) }, u[f].l = 1 * new Date();
+//   b = p.createElement(l), j = p.getElementsByTagName(l)[0];
+//    b.async = 1; b.src = i + '?id=' + t; j.parentNode.insertBefore(b, j);
+//     o = u.location.host.match(/[\w-]+\.\w{2,3}(:\d+)?$/);
+//     if (o) o = o[0];
+//      u[f]('create', t, o) })
+
+//      (window, document, 'script', 'https://cdn.uplift-platform.com/a/up.js', 'up', 'UP-XXXXXXXX-XX');
